b104330652d803e8c7b24e71f7ffe7c069a0c361
ChickCake <owl@ChickCakes-MacBook-Air.local>
2022-09-10 10:56:41 +0800

    Initial Change

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <h1>Yuxuan Xiao</h1>
   </React.StrictMode>
 );
 
