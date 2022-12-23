a3fd92217878f9bdda16fa0728106c770f957361
ChickCake <owl@ChickCakes-MacBook-Air.local>
2022-12-23 23:04:14 +0800

    Updates

---
+++ precache-manifest.09b0a3780af4814206a809916857670d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe8043720b17d973141e",
+    "url": "/static/css/main.7bec722c.chunk.css"
+  },
+  {
+    "revision": "fe8043720b17d973141e",
+    "url": "/static/js/main.7259f06c.chunk.js"
+  },
+  {
+    "revision": "60e406d5dbb3cad0901c",
+    "url": "/static/js/runtime~main.48b17165.js"
+  },
+  {
+    "revision": "37d4b0056f0f0d73b5bd",
+    "url": "/static/js/2.6a99925f.chunk.js"
+  },
+  {
+    "revision": "5854eb9be6dd082bd048",
+    "url": "/static/js/3.524ce66f.chunk.js"
+  },
+  {
+    "revision": "4584136faf89fef09148806aa08af8f3",
+    "url": "/index.html"
+  }
+];
